8691b50bb3137d136f995fa3467c62eb40589bc2
Baran Basaran <baran.basaran@student.griffith.ie>
2022-12-21 21:53:36 +0000

    north line and some comment lines are added

--- app/src/main/java/com/baran/assignment03/TwoDBubbleView.kt
+++ app/src/main/java/com/baran/assignment03/TwoDBubbleView.kt
@@ -19,17 +19,21 @@ class TwoDBubbleView @JvmOverloads constructor(
         color = Color.BLACK
         strokeWidth = 4f
     }
+    private val northLinePaint = Paint().apply {
+        color = Color.RED
+        strokeWidth = 4f
+    }
     private val textPaint = Paint().apply {
         color = Color.BLACK
         textSize = 40f
     }
-
+    // angle at which the bubble is drawn along the x-axis
     var xAngle: Float = 0f
         set(value) {
             field = value
             invalidate()
         }
-
+    // angle at which the bubble is drawn along the y-axis
     var yAngle: Float = 0f
         set(value) {
             field = value
@@ -40,6 +44,7 @@ class TwoDBubbleView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        // get the center x and y coordinates of the view
         val centerX = width / 2f
         val centerY = height / 2f
 
@@ -49,13 +54,14 @@ class TwoDBubbleView @JvmOverloads constructor(
         // Draw y-axis line
         canvas.drawLine(centerX, centerY - 100, centerX, centerY + 100, linePaint)
 
-        // Draw bubble
+        // calculate the x and y coordinates of the bubble based on the x and y angles
         val bubbleRadius = 50f
         val bubbleX = centerX + xAngle / 20 * 100
         val bubbleY = centerY + yAngle / 20 * 100
+        // draw the bubble
         canvas.drawCircle(bubbleX, bubbleY, bubbleRadius, bubblePaint)
 
-        // Draw angle text
+        // draw the x and y angle text
         val xText = "${xAngle.toInt()}°"
         val yText = "${yAngle.toInt()}°"
         val xTextWidth = textPaint.measureText(xText)
@@ -63,6 +69,11 @@ class TwoDBubbleView @JvmOverloads constructor(
         canvas.drawText(xText, centerX - xTextWidth / 2, centerY - bubbleRadius - 10, textPaint)
         canvas.drawText(yText, centerX + bubbleRadius + 10, centerY - yTextWidth / 2, textPaint)
 
+        // Draw line to represent North
+        val northLineLength = 100f
+        val northLineEndX = centerX + northLineLength * Math.sin(Math.toRadians(xAngle.toDouble())).toFloat()
+        val northLineEndY = centerY - northLineLength * Math.cos(Math.toRadians(xAngle.toDouble())).toFloat()
+        canvas.drawLine(centerX, centerY, northLineEndX, northLineEndY, northLinePaint)
 
     }
 }
