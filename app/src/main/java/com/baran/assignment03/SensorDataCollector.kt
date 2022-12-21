c30660e2f31fd6ad3dd7cd6399ee09300a373db9
cmpbasaran <100031393+cmpbasaran@users.noreply.github.com>
2022-12-21 21:53:57 +0000

    updated

--- app/src/main/java/com/baran/assignment03/SensorDataCollector.kt
+++ app/src/main/java/com/baran/assignment03/SensorDataCollector.kt
@@ -6,11 +6,13 @@ import android.hardware.SensorEvent
 import kotlin.math.max
 import kotlin.math.min
 
+// This class is used to collect and store sensor data from the accelerometer and magnetometer sensors
 class SensorDataCollector(private val maxValues: Int) {
 
     private val accelerometerValues = mutableListOf<Float>()
     private val magnetometerValues = mutableListOf<Float>()
 
+    // add new sensor data to the appropriate list
     fun addSensorData(event: SensorEvent) {
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
             addValues(accelerometerValues, event.values)
@@ -18,14 +20,14 @@ class SensorDataCollector(private val maxValues: Int) {
             addValues(magnetometerValues, event.values)
         }
     }
-
+    // helper method to add new values to a list, maintaining the maximum number of values
     private fun addValues(values: MutableList<Float>, newValues: FloatArray) {
         values.addAll(newValues.asIterable())
         while (values.size > maxValues) {
             values.removeAt(0)
         }
     }
-
+    // get the minimum value from the accelerometer and magnetometer sensor data
     fun getMinValue(): Float {
         var minMagnetometerValue: Float? = null
         val minAccelerometerValue = accelerometerValues.min()
@@ -38,6 +40,7 @@ class SensorDataCollector(private val maxValues: Int) {
         }
      return minAccelerometerValue
     }
+    // get max
     fun getMaxValue(): Float {
         val maxAccelerometerValue = accelerometerValues.max()
         var maxMagnetometerValue: Float? = null
@@ -50,9 +53,4 @@ class SensorDataCollector(private val maxValues: Int) {
         }
         return maxAccelerometerValue
     }
-
-    fun getSize(): Int {
-        return accelerometerValues.size
-    }
-
 }
